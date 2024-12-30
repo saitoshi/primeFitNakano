fd1668b4795a12b9aaf6014b7a625d5a69082a0b
Shun <saitoshin2145@gmail.com>
2024-12-30 10:51:36 +0900

    issue-06-added success message

---
+++ src/app/success/page.tsx
@@ -0,0 +1,9 @@
+import type { Metadata } from 'next';
+import { SuccessMsg } from '../_components/ConditionalRelated/SuccessMsg';
+export default function Success() {
+  return (
+    <div>
+      <SuccessMsg />
+    </div>
+  );
+}
