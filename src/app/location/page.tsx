5c1b43c33adc411cf84e074874184fa17a1c3793
Shun <saitoshin2145@gmail.com>
2024-12-30 14:54:25 +0900

    issue-08: added location page

---
+++ src/app/location/page.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from 'next';
+import { LocationCard } from '../_components/LocationCard/LocationCard';
+import { Footer } from '../_components/Footer/Footer';
+export const metadata: Metadata = {
+  metadataBase: new URL('http://localhost:3000'),
+  title: 'Prime Fit Gym 中野 | 店舗情報',
+  keywords: 'パーソナルトレーニング、中野、脱毛、トレーニング、',
+  openGraph: {
+    title: 'Prime Fit Gym 中野 | サービス',
+    type: 'website',
+    images: `/image/mainLogo.png`,
+  },
+};
+export default function Location() {
+  return (
+    <div>
+      <div className='pageContainer'>
+        <h2>
+          <p className='subHeader'>LOCATION</p>
+          <p className='mainHeader'>店舗情報</p>
+        </h2>
+        <LocationCard />
+      </div>
+      <Footer />
+    </div>
+  );
+}
